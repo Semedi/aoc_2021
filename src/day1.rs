ea7b5931b6bc08f13916ddcd6b192104f7efac93
Sergio Semedi <sergio.semedi@gmail.com>
2021-12-02 13:41:30 +0100

    day1

--- src/day1.rs
+++ src/day1.rs
@@ -3,8 +3,43 @@ pub mod helper;
 fn main() {
     if let Some(data) = helper::read_input(1) {
 
-        for number in data.iter() {
-            println!("{}", number);
+        let mut prev = -1; 
+        let mut sum : i32 = 0;
+        let mut cnt = -1;
+
+        // procedural way
+        for (i, number) in data.iter().enumerate() {
+            if i % 3 == 0 {
+                if sum > prev {
+                    cnt+=1;
+                }
+
+                prev = sum;
+                sum = 0;
+            }
+            sum = sum + number;
         }
+        println!("p2: {}", cnt);
+
+        // functional way
+        let p2 = data.iter().fold(
+            (
+                0 as i32,
+                (i32::max_value(), i32::max_value(), i32::max_value()),
+            ),
+            |(cnt, (prev, b, c)), next| 
+                (
+                    cnt + (prev < *next) as i32 ,(b, c, *next)
+                ),
+        )
+        .0;
+
+        println!("p2: {}", p2);
+
+        let t = [0,2,3,4,5]
+        .iter()
+        .fold(0, |total, next| total + next);
+
+        println!("test {}", t);
     }
 }
